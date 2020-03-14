922e80c351c37924080b775c745c5c162576b97c
wblucca <wblucca@wpi.edu>
2020-03-14 00:30:07 -0400

    Clean up JS a little

--- MarkovPainting.js
+++ MarkovPainting.js
@@ -1,3 +1,25 @@
 // Author: William Lucca
 
+const MARGIN_X = 50;
+const MARGIN_Y = 150;
 
+let canvas = document.getElementById('canvas');
+let ctx = canvas.getContext('2d');
+
+/**
+ * Initialize everything
+ */
+function init() {
+	setupCanvas();
+}
+
+/**
+ * Size the canvas to fit the screen well
+ */
+function setupCanvas() {
+	ctx.canvas.width = screen.width - 2 * MARGIN_X;
+	ctx.canvas.height = screen.height - 2 * MARGIN_Y;
+}
+
+// Setup and get started
+window.onload = init;
