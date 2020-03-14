904391203d39581ba13bd951e1bd68e1e83336c9
wblucca <wblucca@wpi.edu>
2020-03-13 20:04:14 -0400

    Add stylesheet and canvas

--- MarkovData.js
+++ MarkovData.js
@@ -1,2 +1,1 @@
-// Author
-// William Lucca
+// Author: William Lucca
--- MarkovPainting.js
+++ MarkovPainting.js
@@ -1,2 +1,5 @@
-// Author
-// William Lucca
+// Author: William Lucca
+
+var canvas = document.getElementById('canvas');
+var ctx = canvas.getContext('2D');
+ctx.fillRect(10, 10, 100, 100);
