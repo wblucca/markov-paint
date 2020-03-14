9ffc723c4b6d58cf7f9a3e40a2fcbef373edd0eb
wblucca <wblucca@wpi.edu>
2020-03-14 01:39:01 -0400

    Improved UI and styles

--- MarkovPainting.js
+++ MarkovPainting.js
@@ -1,7 +1,8 @@
 // Author: William Lucca
 
-const MARGIN_X = 50;
-const MARGIN_Y = 150;
+const SCALE_X = 0.88;
+const SCALE_Y = 0.6;
+const NUM_BRUSHES = 20;
 
 let canvas = document.getElementById('canvas');
 let ctx = canvas.getContext('2d');
@@ -11,14 +12,35 @@ let ctx = canvas.getContext('2d');
  */
 function init() {
 	setupCanvas();
+	createBrushes();
 }
 
 /**
  * Size the canvas to fit the screen well
  */
 function setupCanvas() {
-	ctx.canvas.width = screen.width - 2 * MARGIN_X;
-	ctx.canvas.height = screen.height - 2 * MARGIN_Y;
+	console.log(canvas.width);
+
+	ctx.canvas.width = screen.width * SCALE_X;
+	ctx.canvas.height = screen.height * SCALE_Y;
+	console.log(canvas.width);
+}
+
+/**
+ * Create brush buttons
+ */
+function createBrushes() {
+	// Find the brush list
+	let list = document.getElementById('brush-list');
+
+	// Create each brush button
+	for (let i = 0; i < NUM_BRUSHES; i++) {
+		let brush = document.createElement('li');
+		let brushImage = document.createElement('img');
+		brushImage.src = 'images/empty_brush.png';
+		brush.appendChild(brushImage);
+		list.appendChild(brush);
+	}
 }
 
 // Setup and get started
